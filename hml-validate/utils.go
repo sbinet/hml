efbd2d792f383cffb80d59d71d1af7691876e2bb
Sebastien Binet <binet@cern.ch>
2014-08-14 14:49:03 +0200

    hml-validate: add copytree

---
+++ hml-validate/utils.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+func path_exists(name string) bool {
+	_, err := os.Stat(name)
+	if err == nil {
+		return true
+	}
+	if os.IsNotExist(err) {
+		return false
+	}
+	return false
+}
+
+func copytree(dstdir, srcdir string) error {
+	var err error
+
+	if !path_exists(dstdir) {
+		err = os.MkdirAll(dstdir, 0755)
+		if err != nil {
+			return err
+		}
+	}
+
+	err = filepath.Walk(srcdir, func(path string, info os.FileInfo, err error) error {
+		rel := ""
+		rel, err = filepath.Rel(srcdir, path)
+		out := filepath.Join(dstdir, rel)
+		fmode := info.Mode()
+		if fmode.IsDir() {
+			err = os.MkdirAll(out, fmode.Perm())
+			if err != nil {
+				return err
+			}
+		} else if fmode.IsRegular() {
+			dst, err := os.OpenFile(out, os.O_CREATE|os.O_RDWR, fmode.Perm())
+			if err != nil {
+				return err
+			}
+			defer func() { err = dst.Close() }()
+			src, err := os.Open(path)
+			if err != nil {
+				return err
+			}
+			_, err = io.Copy(dst, src)
+			if err != nil {
+				return err
+			}
+		} else if (fmode & os.ModeSymlink) != 0 {
+			rlink, err := os.Readlink(path)
+			if err != nil {
+				return err
+			}
+			err = os.Symlink(rlink, out)
+			if err != nil {
+				return err
+			}
+		} else {
+			return fmt.Errorf("unhandled mode (%v) for path [%s]", fmode, path)
+		}
+		return nil
+	})
+	return err
+}
