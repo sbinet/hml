3f4e1ec65b2b275666086bb4325235b1ded09c07
Sebastien Binet <binet@cern.ch>
2014-09-12 10:18:29 +0200

    hml-validate: refactor constants

---
+++ hml-validate/consts.go
@@ -0,0 +1,9 @@
+package main
+
+const (
+	buildscript = "higgsml-build" + binExe
+	runscript   = "higgsml-run" + binExe
+	trainscript = "higgsml-train" + binExe
+	trainedname = "trained.dat"
+	csv_results = "submission.csv"
+)
--- hml-validate/hml_windows.go
+++ hml-validate/hml_windows.go
@@ -1,9 +1,5 @@
 package main
 
 const (
-	buildscript = "higgsml-build.bat"
-	runscript   = "higgsml-run.bat"
-	trainscript = "higgsml-train.bat"
-	trainedname = "trained.dat"
-	csv_results = "submission.csv"
+	binExe = ".bat"
 )
