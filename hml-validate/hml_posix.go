25edc08db3cd0ff59c95e96518c60a69a5eeb748
Sebastien Binet <binet@cern.ch>
2014-09-12 10:15:06 +0200

    hml-validate: more magic constants

--- hml-validate/hml_posix.go
+++ hml-validate/hml_posix.go
@@ -1,6 +1,9 @@
 package main
 
 const (
+	buildscript = "higgsml-build"
 	runscript   = "higgsml-run"
 	trainscript = "higgsml-train"
+	trainedname = "trained.dat"
+	csv_results = "submission.csv"
 )
--- hml-validate/hml_windows.go
+++ hml-validate/hml_windows.go
@@ -1,6 +1,9 @@
 package main
 
 const (
+	buildscript = "higgsml-build.bat"
 	runscript   = "higgsml-run.bat"
 	trainscript = "higgsml-train.bat"
+	trainedname = "trained.dat"
+	csv_results = "submission.csv"
 )
